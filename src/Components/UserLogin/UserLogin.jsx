4bb7dd14863c45afef3ff277e85f1f884e7974d1
Lucas <lucasolivah@gmail.com>
2024-02-16 07:12:51 -0300

    User buttons update

--- src/Components/UserLogin/UserLogin.jsx
+++ src/Components/UserLogin/UserLogin.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export const UserLogin = () => {
   return (
     <div className='user-options-wrapper'>
-        <button>Login <i class="bi bi-box-arrow-in-right"></i></button>
-        <button>Register <i class="bi bi-person"></i></button>
+        <button>Sign in <i class="bi bi-box-arrow-in-right"></i></button>
+        <button>Sign up<i class="bi bi-person"></i></button>
     </div>
   )
 }
